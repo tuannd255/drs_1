2e943d4b1e0a81a279046b766fb4584c4a8fa1ed
tuannd255 <tuannd255.com>
2016-06-30 15:02:08 +0700

    finish manage division

---
+++ app/controllers/admin/divisions_controller.rb
@@ -0,0 +1,54 @@
+class Admin::DivisionsController < ApplicationController
+  before_action :logged_in_user
+  before_action :verify_admin
+  before_action :find_division, except: [:create, :new, :index]
+
+  def new
+    @division = Division.new 
+  end
+
+  def create
+    @division = Division.new division_params
+    if @division.save
+      flash[:success] = t "division.create.created"
+      redirect_to admin_divisions_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @division.update_attributes division_params
+      flash[:success] = t "division.updated"
+      redirect_to admin_divisions_path
+    else
+      render :edit
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+    if @division.destroy
+      flash[:success] = t "division.deleted"
+    else
+      flash[:danger] = t "division.delete_fail"
+    end
+    redirect_to admin_divisions_path
+  end
+
+  def index
+    @divisions = Division.paginate page: params[:page]
+  end
+
+  private
+  def division_params
+    params.require(:division).permit :descrition
+  end
+
+  def find_division
+    @division = Division.find_by_id params[:id]
+    redirect_to admin_divisions_path if @division.nil?
+  end
+end
